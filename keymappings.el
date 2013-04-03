4b10dc3bc5831c02df8bb505e299f3f38a97f6c0
Svetlin Simonyan <svetlin.s@gmail.com>
2013-04-03 22:56:08 +0300

    Enable C-w

--- keymappings.el
+++ keymappings.el
@@ -1,6 +1,7 @@
 (global-set-key (kbd "C-x C-m") 'execute-extended-command)
 (global-set-key (kbd "C-x C-k") 'kill-region)
 (global-set-key (kbd "C-x C-b") 'ibuffer)
+(global-set-key (kbd "C-w") 'backward-kill-word)
 
 (add-hook 'ruby-mode-hook
 	  (lambda ()
