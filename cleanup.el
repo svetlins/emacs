5d6c9acb4b79a9b54c8ab845987beaf58ce2287e
Svetlin Simonyan <svetlin.s@gmail.com>
2013-04-04 12:09:39 +0300

    Remove trailing white space on save

--- cleanup.el
+++ cleanup.el
@@ -1,3 +1,6 @@
+;; Some very sensible settings that should have been defaults
+
 (tool-bar-mode -1)
 (setq make-backup-files nil)
 (setq scroll-step 1)
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
