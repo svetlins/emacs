21366f1140a501777936f1661fe4318bf4bc166b
Svetlin Simonyan <svetlin.s@gmail.com>
2013-04-04 16:53:30 +0300

    Add ag.el

--- global-modes.el
+++ global-modes.el
@@ -2,4 +2,6 @@
 	  (lambda ()
 	    (enclose-global-mode)
 	    (drag-stuff-global-mode)
-	    (load-theme 'deep-thought t)))
+	    (load-theme 'deep-thought t)
+            (when (memq window-system '(mac ns))
+              (exec-path-from-shell-initialize))))
--- keymappings.el
+++ keymappings.el
@@ -1,10 +1,12 @@
 (global-set-key (kbd "C-x C-m") 'execute-extended-command)
+(global-set-key (kbd "C-x m") 'execute-extended-command)
 (global-set-key (kbd "C-x C-k") 'kill-region)
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 (global-set-key (kbd "C-w") 'backward-kill-word)
 (global-set-key (kbd "C-y") 'yank-and-indent)
 (global-set-key (kbd "C-,") 'hlt-highlight-region)
 (global-set-key (kbd "C-.") 'hlt-unhighlight-region)
+(global-set-key (kbd "s-f") 'ag-project)
 
 (add-hook 'ruby-mode-hook
 	  (lambda ()
