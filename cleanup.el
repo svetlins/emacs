5f56288241e29ae3d57179e587d61d299fd4ccf1
Svetlin Simonyan <svetlin.s@gmail.com>
2013-04-04 12:41:56 +0300

    Remove startup screen

--- cleanup.el
+++ cleanup.el
@@ -1,6 +1,7 @@
 ;; Some very sensible settings that should have been defaults
 
 (tool-bar-mode -1)
+(setq inhibit-startup-message t)
 (setq make-backup-files nil)
 (setq scroll-step 1)
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
