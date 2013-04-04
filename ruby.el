6ae2a25a325b4db441da3c82490d3a1c5a20f280
Svetlin Simonyan <svetlin.s@gmail.com>
2013-04-04 11:47:08 +0300

    Start ruby mode on more files

--- ruby.el
+++ ruby.el
@@ -2,3 +2,6 @@
 	  (lambda ()
 	    (ruby-end-mode)
 	    (ruby-tools-mode)))
+
+(dolist (regex '("\\.rake$" "\\.gemspec$" "\\.ru$" "Rakefile$" "Gemfile$" "Capfile$" "Guardfile$"))
+  (add-to-list 'auto-mode-alist `(,regex . ruby-mode)))
