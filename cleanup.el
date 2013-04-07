8b0b924ed596f7491966ca762e5a45439e944ac4
Svetlin Simonyan <svetlin.s@gmail.com>
2013-04-07 20:48:26 +0300

    Remove annoying sound

--- cleanup.el
+++ cleanup.el
@@ -6,5 +6,15 @@
 (show-paren-mode 1)
 (setq scroll-step 1)
 (scroll-bar-mode -1)
+(setq-default indent-tabs-mode nil)
 (global-auto-revert-mode 1)
+
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+(setq ring-bell-function
+      (lambda ()
+        (unless (memq this-command
+                      '(isearch-abort abort-recursive-edit exit-minibuffer
+                                      keyboard-quit mwheel-scroll down up next-line previous-line
+                                      backward-char forward-char))
+          (ding))))
